8fb877d418d2c16463bf389a3718de1626a7f863
YAYSUU <yosif.mukhled1@gmail.com>
2026-01-13 04:12:09 +0300

    AAAAHHHH!!! I'M IN 3D!!!!!!!

--- objects/player.gd
+++ objects/player.gd
@@ -10,6 +10,7 @@ var last_animation = ""
 
 var flipped_x = false
 var flipped_z = false
+var runnin = false
 
 const SPEED = 2.0
 #const JUMP_VELOCITY = 4.5
@@ -25,23 +26,24 @@ func set_animation(name: StringName) -> void:
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("game_left", "game_right", "game_up", "game_down") # ooooh how i hate vectors!
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		flipped_z = direction.normalized().z < 0
-		flipped_x = direction.normalized().x < 0
+		runnin = Input.is_action_pressed("game_cancel")
+		velocity.x = direction.x * (SPEED*2 if runnin else SPEED)
+		velocity.z = direction.z * (SPEED*2 if runnin else SPEED) # gotta go FEST!
+		if (velocity.z != 0): # This sucks don't change it
+			flipped_z = velocity.z < 0
+		if (velocity.x != 0):
+			flipped_x = velocity.x < 0
 		set_animation(&"bwalk" if flipped_z else &"fwalk")
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
 	else:
 		set_animation(&"bidle" if flipped_z else &"fidle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
